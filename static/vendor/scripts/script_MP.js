70972ca81ab3bf538e0511dd7f6cf36ee54170c2
DpaNKetTA <orisko67@gmail.com>
2024-10-18 12:47:40 +0300

    добавлен бэк на 2 страницы маршрутов

--- static/vendor/scripts/script_MP.js
+++ static/vendor/scripts/script_MP.js
@@ -6,26 +6,4 @@ $(document).ready(function() {
     });
 });
 
-let currentReview = 0;
-const reviews = document.querySelectorAll('.review');
-
-function showReview(index) {
-    reviews.forEach((review, i) => {
-        review.classList.remove('active');
-        if (i === index) {
-            review.classList.add('active');
-        }
-    });
-}
-
-function nextReview() {
-    currentReview = (currentReview + 1) % reviews.length;
-    showReview(currentReview);
-}
-
-function prevReview() {
-    currentReview = (currentReview - 1 + reviews.length) % reviews.length;
-    showReview(currentReview);
-}
-
 
