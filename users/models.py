1d93d865e92d76628f9a4442a445d8e6c64171db
DpaNKetTA <orisko67@gmail.com>
2024-10-23 19:39:23 +0300

    Заменил дефолтную аватарку

--- users/models.py
+++ users/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User (AbstractUser):
-    image = models.ImageField(upload_to='users_images', null=True, blank=True, default='users_images/img.svg')
+    image = models.ImageField(upload_to='users_images', null=True, blank=True, default='users_images/img.png')
