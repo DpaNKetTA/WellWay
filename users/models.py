8d8ea7be192fe10e0c32732c35bfe54ed7a1552f
DpaNKetTA <orisko67@gmail.com>
2024-10-23 20:03:17 +0300

    убрал перевернутые звезды на 1 маршруте

--- users/models.py
+++ users/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User (AbstractUser):
-    image = models.ImageField(upload_to='users_images', null=True, blank=True, default='users_images/img.png')
+    image = models.ImageField(upload_to='users_images', null=True, blank=True, default='users_images/default.png')
